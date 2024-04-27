e4c69393e03ea534a6c7031f05761f38480a7711
Atmajo <atmajoc@gmail.com>
2024-04-27 11:54:02 +0530

    v1.1.6

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -15,16 +15,6 @@ const Home = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        const res = getDoc(doc(db, "users", user.uid)).then((docSnap) => {
-          if (!docSnap.exists()) {
-            setDoc(doc(db, "users", user.uid), {
-              name: user.displayName,
-              email: user.email,
-              photoURL: user.photoURL,
-              uid: user.uid,
-            });
-          }
-        });
       } else {
         navigate("/signin");
       }
--- src/component/Signin.jsx
+++ src/component/Signin.jsx
@@ -1,16 +1,30 @@
 "use client"
 import React from "react";
-import { auth, provider } from "../firebaseAuth";
+import { auth, provider, app } from "../firebaseAuth";
 import { signInWithPopup, signInWithRedirect } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
+import { doc, setDoc, getFirestore, getDoc } from "firebase/firestore";
 
 const Signin = () => {
+  const db = getFirestore(app);
   const navigate = useNavigate();
+
   const onLogin = async () => {
-    await signInWithPopup(auth, provider);
+    const result = await signInWithPopup(auth, provider);
+    await getDoc(doc(db, "users", result.user.email)).then((docSnap) => {
+      if (!docSnap.exists()) {
+        setDoc(doc(db, "users", result.user.email), {
+          name: result.user.displayName,
+          email: result.user.email,
+          photoURL: result.user.photoURL,
+          uid: result.user.uid,
+        });
+      }
+    });
     navigate("/");
+    console.log(result)
   };
+
   return (
     <div className="flex flex-col justify-center items-center w-screen my-24">
       <div className="w-[60%] md:w-[20%] flex flex-col justify-center items-center my-24 gap-24 p-10 border-2 border-white rounded-lg bg-white/30">
